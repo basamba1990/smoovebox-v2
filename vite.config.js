f36994bd3515fcb935bf8001352ddc7e1733fb3d
basamba1990 <basamba1990@yahoo.fr>
2025-07-21 03:03:15 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    visualizer({ 
-      open: true,
-      filename: 'bundle-analysis.html',
-      gzipSize: true,
-      brotliSize: true,
-    })
   ],
   resolve: {
     alias: {
