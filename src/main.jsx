36665d6e28129e806e94d9a91db8594e9504842b
basamba1990 <basamba1990@yahoo.fr>
2025-08-11 12:17:29 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import AuthCallback from './pages/AuthCallback.jsx'; // Importez votre composant de callback
+import ResetPassword from './pages/ResetPassword.jsx'; // Importez votre composant de réinitialisation de mot de passe
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
