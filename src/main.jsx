1e25450dd3c7046afd2368fa754d97eb63df201d
basamba1990 <basamba1990@yahoo.fr>
2025-09-15 02:23:43 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import AuthCallback from './pages/AuthCallback.jsx'
-import ResetPassword from './pages/ResetPassword.jsx'
-import RecordVideo from './pages/record-video.jsx'
-import VideoSuccess from './pages/video-success.jsx'
-import Directory from './pages/directory.jsx'
-import UserRegistration from './components/UserRegistration.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import AuthCallback from '@/pages/AuthCallback.jsx';
+import ResetPassword from '@/pages/ResetPassword.jsx';
+import RecordVideo from '@/pages/record-video.jsx';
+import VideoSuccess from '@/pages/video-success.jsx';
+import Directory from '@/pages/directory.jsx';
+import UserRegistration from '@components/UserRegistration.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -25,4 +24,4 @@ createRoot(document.getElementById('root')).render(
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
+);
