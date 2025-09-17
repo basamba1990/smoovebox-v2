ea8dff85023ab14ef097941d2ec8ca9baeed409f
basamba1990 <basamba1990@yahoo.fr>
2025-09-17 10:33:54 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import RecordVideo from '@/pages/record-video.jsx';
 import VideoSuccess from '@/pages/video-success.jsx';
 import Directory from '@/pages/directory.jsx';
 import UserRegistration from '@components/UserRegistration.jsx';
-import ProtectedRoute from '@components/ProtectedRoute.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,22 +18,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route
-          path="/record-video"
-          element={
-            <ProtectedRoute>
-              <RecordVideo />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/video-success"
-          element={
-            <ProtectedRoute>
-              <VideoSuccess />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/record-video" element={<RecordVideo />} />
+        <Route path="/video-success" element={<VideoSuccess />} />
         <Route path="/directory" element={<Directory />} />
         <Route path="/register" element={<UserRegistration />} />
       </Routes>
