52ae9911c80ff784e3abaa3eba9279a4a71f65e1
basamba1990 <basamba1990@yahoo.fr>
2025-10-02 11:48:39 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 // tailwind.config.js
-import colors from 'tailwindcss/colors'
-
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        // 🔹 Réactivation des palettes standards
-        gray: colors.gray,
-        blue: colors.blue,
-        red: colors.red,
-        green: colors.green,
-        // 🔹 Tes couleurs personnalisées
         primary: {
           50: '#f0fdfa',
           100: '#ccfbf1',
